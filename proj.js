c8bcf3153cc63c7dc7147f1dccb733fa50cdbf9b
Sanchez-9E-ay2526 <sanchez.bmd@obmontessori.edu.ph>
2026-03-12 20:23:03 +0800

    Update proj.js

--- proj.js
+++ proj.js
@@ -1,1 +1,20 @@
+function calculateFare() {
 
+    var distance = document.getElementById("distance").value;
+    var type = document.getElementById("discount").value;
+
+    if (distance == "" || distance <= 0) {
+        document.getElementById("fareOutput").innerHTML = "Enter a valid distance.";
+        return;
+    }
+
+    var fare = 13 + (distance * 2);
+
+    if (type == "student" || type == "senior" || type == "pwd") {
+        fare = fare * 0.8;
+    }
+
+    fare = fare.toFixed(2);
+
+    document.getElementById("fareOutput").innerHTML = "Fare: ₱" + fare;
+}
